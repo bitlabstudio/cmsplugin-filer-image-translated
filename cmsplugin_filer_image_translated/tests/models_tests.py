df643d7c6ce63e945ccfe3b92fce598e82ba04a3
Daniel Kaufhold <daniel.kaufhold@bitmazk.com>
2014-02-07 13:22:58 +0100

    Creating a old version branch with the test setup

---
+++ cmsplugin_filer_image_translated/tests/models_tests.py
@@ -0,0 +1,8 @@
+"""Tests for the models of the cmsplugin_filer_image_translated app."""
+from django.test import TestCase
+
+
+class DummyTestCase(TestCase):
+    """Just a dummy test to confirm, that the tests are running."""
+    def test_dummy(self):
+        self.assertEqual(1 + 1, 2)
---
+++ cmsplugin_filer_image_translated/tests/test_settings.py
@@ -0,0 +1,122 @@
+"""Settings that need to be set in order to run the tests."""
+import os
+
+gettext = lambda s: s
+
+DEBUG = True
+
+SITE_ID = 1
+
+APP_ROOT = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..'))
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+LANGUAGES = [
+    ('en', gettext('English')),
+]
+
+# just so that we have a login url to redirect to. In the real application,
+# this would of course be different.
+LOGIN_URL = '/admin/'
+
+ROOT_URLCONF = 'cmsplugin_filer_image_translated.tests.test_app.urls'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(APP_ROOT, '../app_static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(APP_ROOT, '../app_media')
+STATICFILES_DIRS = (
+    os.path.join(APP_ROOT, 'static'),
+)
+
+TEMPLATE_DIRS = (
+    os.path.join(APP_ROOT, 'tests/test_app/templates'),
+)
+
+COVERAGE_REPORT_HTML_OUTPUT_DIR = os.path.join(
+    os.path.join(APP_ROOT, 'tests/coverage'))
+COVERAGE_MODULE_EXCLUDES = [
+    'tests$', 'settings$', 'urls$', 'locale$',
+    'migrations', 'fixtures', 'admin$', 'django_extensions',
+]
+
+TEMPLATE_CONTEXT_PROCESSORS = [
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.contrib.messages.context_processors.messages',
+    'django.core.context_processors.request',
+    'cms.context_processors.media',
+    'sekizai.context_processors.sekizai',
+]
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.doc.XViewMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'cms.middleware.page.CurrentPageMiddleware',
+    'cms.middleware.user.CurrentUserMiddleware',
+    'cms.middleware.toolbar.ToolbarMiddleware',
+    'cms.middleware.language.LanguageCookieMiddleware',
+)
+
+EXTERNAL_APPS = [
+    'djangocms_text_ckeditor',
+    'cms',
+    'mptt',
+    'sekizai',
+    'menus',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.messages',
+    'django.contrib.sessions',
+    'django.contrib.staticfiles',
+    'django.contrib.sitemaps',
+    'django.contrib.sites',
+    'django_nose',
+    'filer',
+    'cmsplugin_filer_file',
+    'cmsplugin_filer_folder',
+    'cmsplugin_filer_image',
+    'cmsplugin_filer_teaser',
+    'cmsplugin_filer_video',
+    'easy_thumbnails',
+]
+
+INTERNAL_APPS = [
+    'cmsplugin_filer_image_translated',
+    'cmsplugin_filer_image_translated.tests.test_app',
+]
+
+INSTALLED_APPS = EXTERNAL_APPS + INTERNAL_APPS
+COVERAGE_MODULE_EXCLUDES += EXTERNAL_APPS
+
+SECRET_KEY = 'foobar'
+
+CMS_TEMPLATES = (
+    ('cms/default.html', 'Default'),
+)
+
+THUMBNAIL_PROCESSORS = (
+    'easy_thumbnails.processors.colorspace',
+    'easy_thumbnails.processors.autocrop',
+    #'easy_thumbnails.processors.scale_and_crop',
+    'filer.thumbnail_processors.scale_and_crop_with_subject_location',
+    'easy_thumbnails.processors.filters',
+)
