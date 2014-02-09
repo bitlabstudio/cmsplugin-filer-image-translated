4c43e9d9a6966487b61d2ed42a2b32ab4d5cfdd3
Daniel Kaufhold <daniel.kaufhold@bitmazk.com>
2014-02-09 08:15:07 +0100

    Migrated to hvad.

--- cmsplugin_filer_image_translated/simple_translate.py
+++
@@ -1,9 +0,0 @@
-"""Registering translated models for the ``filer_image_translated`` app."""
-from simple_translation.translation_pool import translation_pool
-
-from filer.models import Image
-
-from .models import ImageTranslation
-
-
-translation_pool.register_translation(Image, ImageTranslation)
